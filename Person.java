d6dbb762031020e3da3ce90e226a04792e1bd12d
Laiba Nadeem <miss.laiba.nadeem@gmail.com>
2026-07-31 13:50:49 +0500

    Upload person.java

---
+++ Person.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bse5a2_l227998_laibanadeem_q2;
+
+/**
+ *
+ * @author fast
+ */
+public class Person {
+    public String name;
+    public String id;
+   
+}
